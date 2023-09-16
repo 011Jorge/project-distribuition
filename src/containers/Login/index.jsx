f6a775cd70bbbfe6bca9b9189608a7706cfba489
011Jorge <vicentejorge187@gmail.com>
2023-09-15 22:11:10 -0300

    :construction: update login page

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,25 +1,49 @@
-import ImgLogin from "../../assets/imgLogin.jpg";
+import { FaUserAlt, FaArrowRight } from "react-icons/fa";
+import { GiPadlock } from "react-icons/gi";
 
-import { ContainerLogin, ContainerImage, ContainerInfo } from "./style";
+import {
+  ContainerLogin,
+  ContainerForm,
+  ContainerImageForm,
+  ContainerInfoForm,
+} from "./style";
+
+import Image from "../../assets/imgLogin.jpg";
 
 function Login() {
   return (
     <ContainerLogin>
-      <ContainerImage>
-        <img src={ImgLogin} alt="image-login" />
-      </ContainerImage>
-      <ContainerInfo>
-        <h1>Login</h1>
-        <input type="text" placeholder="Email" />
-        <input
-          type="password"
-          name="inputPassword"
-          id="inputPassword"
-          placeholder="Senha"
-        />
-        <a href="#">Esqueci minha senha</a>
-        <button>Entrar</button>
-      </ContainerInfo>
+      <ContainerForm>
+        <ContainerImageForm>
+          <img src={Image} alt="image-login" />
+        </ContainerImageForm>
+        <ContainerInfoForm>
+          <form>
+            <h1>Login</h1>
+            <label>
+              <FaUserAlt style={{ marginRight: 10 }} />
+              Email
+            </label>
+
+            <input type="email" placeholder="Digite seu email..." />
+            <label>
+              <GiPadlock style={{ marginRight: 10 }} />
+              Senha
+            </label>
+            <input type="password" placeholder="Digite sua senha..." />
+            <a href="#" id="link-password">
+              Esqueci minha senha!
+            </a>
+            <button style={{ marginTop: 75, marginBottom: 25 }}>
+              Sign In <FaArrowRight style={{ margin: 10 }} />
+            </button>
+
+            <a href="#">
+              Ainda não possui uma conta? <br /> Crie uma agora mesmo!
+            </a>
+          </form>
+        </ContainerInfoForm>
+      </ContainerForm>
     </ContainerLogin>
   );
 }
--- src/containers/Login/style.js
+++ src/containers/Login/style.js
@@ -5,47 +5,112 @@ export const ContainerLogin = styled.div`
   height: 100vh;
 
   display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
 
-  background: #c62368;
+  background-color: #8b4869;
 `;
 
-export const ContainerImage = styled.div`
-  width: 700px;
-  height: 900px;
-  background: #fafafa;
-  border-radius: 30px 0 0 30px;
+export const ContainerForm = styled.div`
+  width: 1500px;
+  height: 800px;
+
+  display: flex;
+
+  background-color: #f4e9ed;
+
+  border-radius: 30px;
+`;
+
+export const ContainerImageForm = styled.div`
+  width: 50%;
+  height: 100%;
 
   img {
     width: 700px;
-    height: 900px;
+    height: 800px;
 
     border-radius: 30px 0 0 30px;
   }
 `;
 
-export const ContainerInfo = styled.div`
-  width: 700px;
-  height: 900px;
+export const ContainerInfoForm = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 
-  background: #fafafa;
+  margin-left: 150px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
 
   h1 {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 50px;
+    line-height: 28px;
+    color: #000;
     text-align: center;
-    font-size: 45px;
+    margin-top: 50px;
+    margin-bottom: 50px;
+  }
+
+  label {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 14px;
+    color: #000;
+    margin-top: 28px;
+    margin-bottom: 20px;
   }
 
   input {
+    width: 400px;
+    height: 60px;
+    background: #ffffff;
+    box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+    border-radius: 5px;
+    border: none;
+    padding-left: 10px;
+    font-size: 17px;
+  }
+
+  button {
+    font-size: 18px;
+    height: 60px;
+    border: none;
+    border-radius: 10px;
+    background-color: #8b4869;
+    color: #fff;
+    cursor: pointer;
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin: 0 auto;
+    text-align: center;
 
-    width: 500px;
-    height: 50px;
-    border-radius: 10px;
-    border: none;
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+
+  a {
+    text-align: center;
+    color: #8b4869;
+
+    &:hover {
+      color: blue;
+    }
+  }
+
+  #link-password {
+    text-align: left !important;
+    margin-top: 20px;
   }
 `;
